1c3149fda5dfdaa05a5f940a72233827bf4c2b1e
mirkotorrisi <mirko.torrisi92@gmail.com>
2021-04-11 17:59:11 +0200

    fixed

--- routes/slot.js
+++ routes/slot.js
@@ -25,10 +25,11 @@ async ({ body: { numOfWheels,numOfSymbols, betImport }, user: { id } }, res) => 
     await decrementUserSum(betImport, id);
     const results = Array.from({length: numOfWheels}, () => Math.floor(Math.random() * 100));
     const values = results.map((i) => i % numOfSymbols)
+    let sum;
     let duplicates = values.filter((item, index) => values.indexOf(item) != index)
         if(duplicates.length > 0){
             const points = [-100, 1,5,10,5,5,5,50,10,7,5]
-            const sum = duplicates.length === numOfWheels-1 ? points[duplicates[0]]**numOfWheels : points[duplicates[0]] * (duplicates.length+1)
+            sum = duplicates.length === numOfWheels-1 ? points[duplicates[0]]**numOfWheels : points[duplicates[0]] * (duplicates.length+1)
             incrementUserSum(sum, id)
         }
       res.json({
